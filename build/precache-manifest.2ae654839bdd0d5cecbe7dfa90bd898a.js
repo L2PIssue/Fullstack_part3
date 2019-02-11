a9672212c71001445d7a6c7bf078da76cfe63957
Miiu <miia.ramo@helsinki.fi>
2019-02-11 16:20:03 +0200

    frontend to production

---
+++ build/precache-manifest.2ae654839bdd0d5cecbe7dfa90bd898a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44be65471e034a3d5a44",
+    "url": "/static/js/main.44be6547.chunk.js"
+  },
+  {
+    "revision": "0f525a57f0991ad915c9",
+    "url": "/static/js/1.0f525a57.chunk.js"
+  },
+  {
+    "revision": "44be65471e034a3d5a44",
+    "url": "/static/css/main.058a4694.chunk.css"
+  },
+  {
+    "revision": "c899823f14afe91e42952d9f063eb840",
+    "url": "/index.html"
+  }
+];
