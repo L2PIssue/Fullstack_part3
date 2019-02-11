b3ab84a6f0b669c8b081e629025a52b8d0018387
Miiu <miia.ramo@helsinki.fi>
2019-02-11 20:27:07 +0200

    first functionalities with MongoDB(3.12)

--- mongo.js
+++ mongo.js
@@ -16,8 +16,9 @@ const Person = mongoose.model('Person', personSchema)
 
 if ( process.argv.length<4 ) {
   Person.find({}).then(result => {
+    console.log('puhelinluettelo:')
     result.forEach(person => {
-      console.log(person)
+      console.log(person.name + ' ' + person.number)
     })
     mongoose.connection.close()
   })
