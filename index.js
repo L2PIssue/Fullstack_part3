5897ec2f6ee6d06570897c7830a287ebea67826a
Miiu <miia.ramo@helsinki.fi>
2019-02-11 14:01:21 +0200

    3.6 completed

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const http = require('http')
+var time = require('express-timestamp')
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   {
-    "name": "Joku Nimi",
+    "name": "Toimiikohan Nodemon",
     "number": "0501234567",
     "id": 1
   },
@@ -18,11 +24,64 @@ let persons = [
   }
 ]
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })  
-  res.end(JSON.stringify(persons))
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+  date = new Date()
+  text = "<p>Puhelinluettelossa on " 
+        + persons.length 
+        + " henkilön tiedot</p>"
+        + "<p>"
+        + date
+        + "</p>"
+  res.send(text)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)  
+  } else {    
+    res.status(404).end()  
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  res.status(204).end();
 });
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  // Check if name already in the book
+  if (persons.some(e => e.name === body.name)) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  // Check if name and number are given
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({ 
+      error: 'both name and number must be given' 
+    })
+  }
+
+  persons = persons.concat(body)
+  res.json(body)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
